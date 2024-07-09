2ed54d532b1b3097fd2c931563667ec9eaa5e8e0
Mahad Alam <mahadalam3@gmail.com>
2024-07-09 21:32:31 +0500

    commit

--- test/src/main.js
+++ test/src/main.js
@@ -1,4 +1,6 @@
 // src/main.js
+import firebase from  'firebase/app';
+import 'firebase/storage';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -12,3 +14,14 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 app.mount('#app');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDq59rjrBQlTqCcd9qa2BViEEY1HyTEXgo",
+    authDomain: "bookworm-a1823.firebaseapp.com",
+    projectId: "bookworm-a1823",
+    storageBucket: "bookworm-a1823.appspot.com",
+    messagingSenderId: "65926355935",
+    appId: "1:65926355935:web:e3a66237ffee4de62b826a",
+    measurementId: "G-ZRSNZ6LRWB"
+  };
+firebase.initializeApp(firebaseConfig)
